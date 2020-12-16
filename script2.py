f6fd50e83bb1d4ea537aef6fc08e08094751bd48
GalMiles <58370322+GalMiles@users.noreply.github.com>
2020-12-16 23:52:38 +0200

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,8 +1,50 @@
 import requests
+import json
+import pandas as pd
 
-url = 'https://disease.sh/v3/covid-19/historical/Israel?lastdays=30'
-myobj = {'12/3/20': '12/9/20'}
+method =['newCasesPeak', 'deathsPeak','recoveredPeak','status']
 
-x = requests.post(url, data = myobj)
+def main():
+    query = input()
+    #geting to the method
+    address = query[20:len(query)]
+    newAddress = address.split("?")
+    country = newAddress[1].strip("country=")
 
-print(x.text)
+    url = "https://disease.sh/v3/covid-19/historical/"+country+"?lastdays=30"
+    print(url)
+    x =requests.get(url)
+
+    y = x.json()
+
+    #newCases
+    if(newAddress[0] == method[0]):
+        df = pd.DataFrame(y['timeline'])
+        casesPeakDate = df.cases.idxmax()
+        casesPeakValue = df.cases.max()
+        print("{'country':"+country+",'method':newCasesPeak,'date':"+casesPeakDate, end='')
+        print(" ,'value':",end='')
+        print(casesPeakValue, end='')
+        print("}")
+
+        #deathCase
+    elif(newAddress[0] == method[1]):
+        df = pd.DataFrame(y['timeline'])
+        deathsPeakDate = df.deaths.idxmax()
+        deathsPeakValue = df.deaths.max()
+        print("{'country':"+country+",'method':deathsPeak,'date':"+deathsPeakDate, end='')
+        print(" ,'value':",end='')
+        print(deathsPeakValue, end='')
+        print("}")
+
+
+    recoverPeak = df.recovered.max()
+    #print(df.cases.idxmax())
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
