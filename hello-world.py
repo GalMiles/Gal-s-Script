8d8632761dffd5fc4193c64fc29abd512987c05b
GalMiles <58370322+GalMiles@users.noreply.github.com>
2020-12-18 01:12:45 +0200

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,15 +1,37 @@
-from flask import Flask
-app = Flask(__name__)
+import requests
+import json
+import pandas as pd
+from flask import Flask, redirect, url_for, request
+method =['newCasesPeak', 'deathsPeak','recoveredPeak','status']
+countries =['israel', 'england']
 
-@app.route('/', method=['GET'])
-def hello_world():
-    return 'Hello, World!'
+app = flask.Flask(_name_)
+#app.config["DEBUG"] = True
 
-@app.route('/<home>/')
-def hello(home):
-    return 'Hello' + home
+@app.route('/') #empty
+def empty():
+    return '{}'
+
+@app.route('/status') #status
+def status():
+     return '{“status”: “success”}'
 
 
-if __name__ =="__main__":
+@app.route('/newCasesPeak?country=israel') #newCasesPeak
+def newCasesPeak():
+    url = "https://disease.sh/v3/covid-19/historical/israel?lastdays=30"
+    x = requests.get(url)
+    y = x.json()
+    df = pd.DataFrame(y['timeline'])
+    casesPeakDate = df.cases.idxmax()
+    casesPeakValue = df.cases.max()
+    return '{“country”:”israel”,“method”:“newCasesPeak”,”date”:"' +casesPeakValue +'",“value”:' + newCasesPeak
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
+
+
 
