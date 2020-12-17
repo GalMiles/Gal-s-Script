815230cecf48c50a8e3e1362ab40262c6f8ff553
GalMiles <58370322+GalMiles@users.noreply.github.com>
2020-12-17 19:56:57 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,10 @@
-import flask
+from flask import Flask
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Hello, World!"
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
