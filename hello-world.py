b6e05e7993d51fbb55983ca7cf3393d3c5f47c6f
GalMiles <58370322+GalMiles@users.noreply.github.com>
2020-12-17 22:47:57 +0200

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,10 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
-@app.route('/home', methods=['GET'])
+
+@app.route('/home')
 def home():
     return 'your home'
 app.run()
