a36679ce1179401f718e2a5c8f98db9a1882534e
GalMiles <58370322+GalMiles@users.noreply.github.com>
2020-12-18 01:56:27 +0200

    Update myScriptWithFlask.py

--- myScriptWithFlask.py
+++ myScriptWithFlask.py
@@ -17,16 +17,6 @@ def status():
      return '{“status”: “success”}'
 
 
-@app.route('/newCasesPeak?country=israel', methods=['GET']) #newCasesPeak
-def newCasesPeak():
-    url = "https://disease.sh/v3/covid-19/historical/israel?lastdays=30"
-    x = requests.get(url)
-    y = x.json()
-    df = pd.DataFrame(y['timeline'])
-    casesPeakDate = df.cases.idxmax()
-    casesPeakValue = df.cases.max()
-    return '{“country”:”israel”,“method”:“newCasesPeak”,”date”:"' +casesPeakValue +'",“value”:' + newCasesPeak
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
